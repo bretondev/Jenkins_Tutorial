58349a30e8fa00ef33dd7306f8df708bfb6ce6a3
bretondev <33033418+bretondev@users.noreply.github.com>
2025-03-15 16:49:11 +0100

    Update Calculator_Test.java

--- java-calculator-app/src/test/java/com/marcobouin/Calculator_Test.java
+++ java-calculator-app/src/test/java/com/marcobouin/Calculator_Test.java
@@ -4,6 +4,9 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import com.marcobouin.Calculator; 
+
+
 public class Calculator_Test {
 
     private Calculator calculator;
