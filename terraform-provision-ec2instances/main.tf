a4ac16a9f70c485c84cdeff911168cff8a93b9b0
bretondev <33033418+bretondev@users.noreply.github.com>
2025-07-04 10:29:42 +0200

    Update main.tf

--- terraform-provision-ec2instances/main.tf
+++ terraform-provision-ec2instances/main.tf
@@ -4,10 +4,10 @@ provider "aws" {
 
 resource "aws_instance" "example" {
   count         = 2
-  ami           = "ami-0c55b159cbfafe1f0"   # Amazon Linux 2 in us-east-1 (update as needed)
+  ami           = "ami-0f8d3c5dcfaceaa4f"
   instance_type = "t2.micro"
 
   tags = {
-    Name = "Terraform-EC2-${count.index + 1}"
+    Name = "provisioned-by-jenkins"
   }
 }
